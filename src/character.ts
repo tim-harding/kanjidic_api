d778b12aea3cb9d3182c2cb16727d15db9dd27ea
Tim Harding <Tim@TimHarding.co>
2021-08-26 21:51:22 -0700

    Initial pass at Character

---
+++ src/character.ts
@@ -0,0 +1,74 @@
+// /// Information about a kanji.
+// #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")]
+// pub struct Character {
+//     /// The character itself.
+//     pub literal: String,
+//     /// Alternate encodings for the character.
+//     #[serde(skip_serializing_if = "Vec::is_empty")]
+//     pub codepoints: Vec<Codepoint>,
+//     /// Alternate classifications for the character by radical.
+//     #[serde(skip_serializing_if = "Vec::is_empty")]
+//     pub radicals: Vec<Radical>,
+//     /// The kanji grade level.
+//     #[serde(skip_serializing_if = "Option::is_none")]
+//     pub grade: Option<Grade>,
+//     /// The stroke count of the character.
+//     pub stroke_counts: StrokeCount,
+//     /// Cross-references to other characters or alternative indexings.
+//     #[serde(skip_serializing_if = "Vec::is_empty")]
+//     pub variants: Vec<Variant>,
+//     /// A ranking of how often the character appears in newspapers.
+//     #[serde(skip_serializing_if = "Option::is_none")]
+//     pub frequency: Option<u16>,
+//     /// The kanji's name as a radical if it is one.
+//     #[serde(skip_serializing_if = "Vec::is_empty")]
+//     pub radical_names: Vec<String>,
+//     /// Old JLPT level of the kanji. Based on pre-2010 test levels
+//     /// that go up to four, not five.
+//     #[serde(skip_serializing_if = "Option::is_none")]
+//     pub jlpt: Option<u8>,
+//     /// Indexes into dictionaries and other instructional books
+//     #[serde(skip_serializing_if = "Vec::is_empty")]
+//     pub references: Vec<Reference>,
+//     /// Codes used to identify the kanji
+//     #[serde(skip_serializing_if = "Vec::is_empty")]
+//     pub query_codes: Vec<QueryCode>,
+//     /// Different ways the kanji can be read.
+//     #[serde(skip_serializing_if = "Vec::is_empty")]
+//     pub readings: Vec<Reading>,
+//     /// Translations of the kanji into different languages.
+//     pub translations: Translations,
+//     /// Japanese readings associated with names.
+//     #[serde(skip_serializing_if = "Vec::is_empty")]
+//     pub nanori: Vec<String>,
+//     /// The constituent radicals in the kanji
+//     #[serde(skip_serializing_if = "Vec::is_empty")]
+//     pub decomposition: Vec<String>,
+// }
+
+import { Codepoint } from "./codepoint";
+import { Grade } from "./grade";
+import { Radical } from "./radical";
+import { Reference } from "./reference";
+import { StrokeCount } from "./stroke_count";
+import { Uint } from "./uint";
+import { Variant } from "./variant";
+
+export interface Character {
+	literal: string
+	codepoints: Array<Codepoint>
+	radicals: Array<Radical>
+	grade: Grade | undefined
+	strokeCounts: StrokeCount
+	variants: Array<Variant>
+	frequency: Uint | undefined
+	radicalNames: Array<string>
+	jlpt: Uint | undefined
+	references: Array<Reference>
+	// queryCodes: Array<QueryCode>
+	// readings: Array<Reading>
+	// translations: Translations
+	nanori: Array<string>
+	decomposition: Array<string>
+}
---
+++ src/codepoint.ts
@@ -0,0 +1,52 @@
+import { Kuten } from "./kuten";
+import { Uint } from "./uint";
+
+/**
+ * Encoding in JIS X 0208-1997
+ */
+export type Jis208 = "Jis208"
+
+/**
+ * Encoding in JIS X 0212-1990
+ */
+export type Jis212 = "Jis212"
+
+/**
+ * Encoding in JIS X 0213-2000
+ */
+export type Jis213 = "Jis213"
+
+/**
+ * A codepoint in kuten coding. 
+ */
+export interface Codepoint_Kuten {
+	/**
+	 * The JIS encoding standard.
+	 */
+	kind: Jis208 | Jis212 | Jis213
+
+	/**
+	 * The encoding.
+	 */
+	value: Kuten
+}
+
+/**
+ * Unicode character
+ */
+export interface Codepoint_Unicode {
+	/**
+	 * The codepoint kind.
+	 */
+	kind: "Unicode"
+
+	/**
+	 * The codepoint.
+	 */
+	value: Uint
+}
+
+/**
+ * The code of a kanji in a given character set standard.
+ */
+export type Codepoint = Codepoint_Kuten | Codepoint_Unicode
