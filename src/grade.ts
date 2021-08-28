976b544c0bf2c06d2ebb8a954a6a88686b4978f4
Tim Harding <Tim@TimHarding.co>
2021-08-27 23:00:35 -0700

    Grade unknown

--- src/grade.ts
+++ src/grade.ts
@@ -1,24 +1,86 @@
+import { isNumber, isObject, tryGetProperty, tryGetString } from "./shared";
 import { Uint } from "./uint"
 
-/**
- * The grade in which a kanji is introduced to Japanese children.
- */
-export type Grade = Grade_Kyouiku | Grade_Other;
+export namespace Grade {
+	/**
+	 * The grade in which a kanji is introduced to Japanese children.
+	 */
+	export type Grade = Grade_Kyouiku | Grade_Other;
 
-/**
- * A Kyouiku kanji learned in grades 1-6. 
- */
-export interface Grade_Kyouiku {
-	tag: "Kyouiku"
-	grade: Uint
-}
+	/**
+	 * A Kyouiku kanji learned in grades 1-6. 
+	 */
+	export interface Grade_Kyouiku {
+		tag: "Kyouiku"
+		grade: Uint
+	}
 
-/**
- * Depending on the `kind`, this represents a kanji that is learned in one of the following:
- * - `Jouyou`: A remaining Jouyou kanji to be learned in junior hi-school.
- * - `Jinmeiyou` A Jinmeiyou kanji for use in names that is approved for use in family name registers and other official documents.
- * - `JinmeiyouJouyouVariant`: A Jinmeiyou kanji that is a variant of a Jouyou kanji.
- */
-export interface Grade_Other {
-	tag: "Jouyou" | "Jinmeiyou" | "JinmeiyouJouyouVariant"
+	/**
+	 * A remaining Jouyou kanji to be learned in junior hi-school.
+	 */
+	export type Jouyou = "Jouyou"
+
+	/**
+	 * A Jinmeiyou kanji for use in names that is approved for use in 
+	 * family name registers and other official documents.
+	 */
+	export type Jinmeiyou = "Jinmeiyou"
+
+	/**
+	 * A Jinmeiyou kanji that is a variant of a Jouyou kanji.
+	 */
+	export type JinmeiyouJouyouVariant = "JinmeiyouJouyouVariant"
+
+	export type GradeOtherTags = Jouyou |
+		Jinmeiyou |
+		JinmeiyouJouyouVariant
+
+	const GRADE_OTHER_TAGS: Record<GradeOtherTags, boolean> = {
+		"Jouyou": true,
+		"Jinmeiyou": true,
+		"JinmeiyouJouyouVariant": true,
+	}
+	
+	function isGradeOtherTag(tag: string): tag is GradeOtherTags {
+		return tag in GRADE_OTHER_TAGS
+	}
+	
+	/**
+	 * Represents a kanji that is learned outside grades 1-6
+	 */
+	export interface Grade_Other {
+		tag: GradeOtherTags
+	}
+	
+	export function fromUnknown(value: unknown): Grade | Error {
+		if (!isObject(value)) {
+			return new Error("Value is not an object")
+		}
+		const tag = tryGetString(value, "tag")
+		if (tag instanceof Error) {
+			return tag
+		}
+		if (isGradeOtherTag(tag)) {
+			return {
+				tag,
+			}
+		} else if (tag === "Kyouiku") {
+			const gradeRaw = tryGetProperty(value, "grade")
+			if (gradeRaw instanceof Error) {
+				return gradeRaw
+			}
+			if (!isNumber(gradeRaw)) {
+				return new Error("Grade property is not a number")
+			}
+			const grade = Uint.new(gradeRaw)
+			if (grade instanceof Error) {
+				return grade
+			}
+			return {
+				tag,
+				grade,
+			}
+		}
+		return new Error("Tag was not a valid Grade variant")
+	}
 }
