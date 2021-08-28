61fc191c9e1a5d4aeaae0e7ed00d840f9cc37815
Tim Harding <Tim@TimHarding.co>
2021-08-28 16:35:00 -0700

    Four Corner type guard

--- src/four_corner.ts
+++ src/four_corner.ts
@@ -1,107 +1,64 @@
-import { isObject, tryGetProperty } from "./shared"
-import { Stroke } from "./stroke"
+import { hasProperty, isObject } from "./shared"
+import { isStroke, Stroke } from "./stroke"
 
-export namespace FourCorner {
+/**
+ * A kanji classification in the Four Corner Coding system. See
+ * http://www.edrdg.org/wwwjdic/FOURCORNER.html
+ * for more information.
+ */
+export interface FourCorner {
 	/**
-	 * A kanji classification in the Four Corner Coding system. See
-	 * http://www.edrdg.org/wwwjdic/FOURCORNER.html
-	 * for more information.
+	 * The stroke at the top left corner.
 	 */
-	export interface FourCorner {
-		/**
-		 * The stroke at the top left corner.
-		 */
-		topLeft: Stroke.Stroke
-
-		/**
-		 * The stroke at the top right corner.
-		 */
-		topRight: Stroke.Stroke
-
-		/**
-		 * The stroke at the bottom left corner.
-		 */
-		bottomLeft: Stroke.Stroke
-
-		/**
-		 * The stroke at the bottom right corner.
-		 */
-		bottomRight: Stroke.Stroke
-
-		/**
-		 * Where necessary to differentiate between other
-		 * characters with the same strokes, this extra stroke
-		 * is found above the bottom right stroke.
-		 */
-		fifthCorner: Stroke.Stroke | undefined
-	}
+	topLeft: Stroke
 
 	/**
-	 * Converts a four corner code to a string.
-	 * @param fourCorner The four corner code
-	 * @returns The string
+	 * The stroke at the top right corner.
 	 */
-	export function serialize(fourCorner: FourCorner): string {
-		const { topLeft, topRight, bottomLeft, bottomRight, fifthCorner } = fourCorner
-		if (fourCorner.fifthCorner === undefined) {
-			return `${topLeft}${topRight}${bottomLeft}${bottomRight}`
-		} else {
-			return `${topLeft}${topRight}${bottomLeft}${bottomRight}.${fifthCorner}`
-		}
-	}
+	topRight: Stroke
 
-	export function fromUnknown(value: unknown): FourCorner | Error {
-		if (!isObject(value)) {
-			return new Error("Value is not an object")
-		}
-		const topLeft = tryGetStroke(value, "topLeft")
-		if (topLeft instanceof Error) {
-			return topLeft
-		}
-		const topRight = tryGetStroke(value, "topRight")
-		if (topRight instanceof Error) {
-			return topRight
-		}
-		const bottomLeft = tryGetStroke(value, "bottomLeft")
-		if (bottomLeft instanceof Error) {
-			return bottomLeft
-		}
-		const bottomRight = tryGetStroke(value, "bottomRight")
-		if (bottomRight instanceof Error) {
-			return bottomRight
-		}
-		const fifthCorner = tryGetOptionalStroke(value, "fifthCorner")
-		if (fifthCorner instanceof Error) {
-			return fifthCorner
-		}
-		return {
-			topLeft,
-			topRight,
-			bottomLeft,
-			bottomRight,
-			fifthCorner,
-		}
-	}
-	
-	function tryGetOptionalStroke(value: object, propertyName: string): Stroke.Stroke | undefined | Error {
-		const property = tryGetProperty(value, propertyName)
-		if (property instanceof Error) {
-			return undefined
-		}
-		if (!Stroke.isStroke(property)) {
-			return new Error("Value is not a stroke variant")
-		}
-		return property
-	}
+	/**
+	 * The stroke at the bottom left corner.
+	 */
+	bottomLeft: Stroke
 
-	function tryGetStroke(value: object, propertyName: string): Stroke.Stroke | Error {
-		const property = tryGetProperty(value, propertyName)
-		if (property instanceof Error) {
-			return property
-		}
-		if (!Stroke.isStroke(property)) {
-			return new Error("Value is not a stroke variant")
-		}
-		return property
+	/**
+	 * The stroke at the bottom right corner.
+	 */
+	bottomRight: Stroke
+
+	/**
+	 * Where necessary to differentiate between other
+	 * characters with the same strokes, this extra stroke
+	 * is found above the bottom right stroke.
+	 */
+	fifthCorner?: Stroke
+}
+
+/**
+ * Converts a four corner code to a string.
+ * @param fourCorner The four corner code
+ * @returns The string
+ */
+export function serializeFourCorner(fourCorner: FourCorner): string {
+	const { topLeft, topRight, bottomLeft, bottomRight, fifthCorner } = fourCorner
+	if (fifthCorner === undefined) {
+		return `${topLeft}${topRight}${bottomLeft}${bottomRight}`
+	} else {
+		return `${topLeft}${topRight}${bottomLeft}${bottomRight}.${fifthCorner}`
 	}
+}
+
+export function isFourCorner(value: unknown): value is FourCorner {
+	return isObject(value) &&
+		hasStrokeProperty(value, "topLeft") &&
+		hasStrokeProperty(value, "topRight") &&
+		hasStrokeProperty(value, "bottomLeft") &&
+		hasStrokeProperty(value, "bottomRight") &&
+		(!hasProperty(value, "fifthCorner") || isStroke(value.fifthCorner))
+}
+
+export function hasStrokeProperty<K extends string, T extends object>(value: T, key: K): value is T & Record<K, Stroke> {
+	return hasProperty(value, key) && 
+		isStroke(value[key])
 }
--- src/stroke.ts
+++ src/stroke.ts
@@ -1,48 +1,45 @@
-import { isNumber } from "./shared"
+import { isInt } from "./uint"
 
-export namespace Stroke {
-	/**
-	 * A stroke in the Four Corner kanji classification system.
-	 */
-	export const enum Stroke {
-		Lid,
-		LineHorizontal,
-		LineVertical,
-		Dot,
-		Cross,
-		Skewer,
-		Box,
-		Angle,
-		Hachi,
-		Chiisai,
-	}
-
-	export function isStroke(value: unknown): value is Stroke {
-		return isNumber(value) &&
-			Number.isInteger &&
-			value >= Stroke.Lid && 
-			value <= Stroke.Chiisai
-	}
-
-	/**
-	 * Gets a character representing a stroke's shape. 
-	 * @param stroke The stroke
-	 * @returns The string for the shape
-	 */
-	export function shape(stroke: Stroke): string {
-		return MAPPING[stroke as number] as string
-	}
-
-	const MAPPING = [
-		"亠",
-		"一",
-		"｜",
-		"丶",
-		"十",
-		"キ",
-		"口",
-		"厂",
-		"八",
-		"小",
-	]
+/**
+ * A stroke in the Four Corner kanji classification system.
+ */
+export const enum Stroke {
+	Lid,
+	LineHorizontal,
+	LineVertical,
+	Dot,
+	Cross,
+	Skewer,
+	Box,
+	Angle,
+	Hachi,
+	Chiisai,
 }
+
+export function isStroke(value: unknown): value is Stroke {
+	return isInt(value) &&
+		value >= Stroke.Lid &&
+		value <= Stroke.Chiisai
+}
+
+/**
+ * Gets a character representing a stroke's shape. 
+ * @param stroke The stroke
+ * @returns The string for the shape
+ */
+export function shape(stroke: Stroke): string {
+	return MAPPING[stroke as number] as string
+}
+
+const MAPPING = [
+	"亠",
+	"一",
+	"｜",
+	"丶",
+	"十",
+	"キ",
+	"口",
+	"厂",
+	"八",
+	"小",
+]
