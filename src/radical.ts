97543463fa42338ba4f52dfb8e2641718ce2991b
Tim Harding <Tim@TimHarding.co>
2021-08-28 19:07:52 -0700

    Radical type guard

--- src/radical.ts
+++ src/radical.ts
@@ -1,4 +1,7 @@
-import { Kangxi } from "./kangxi";
+import { isKangxi, Kangxi } from "./kangxi";
+import { hasProperty, hasStringProperty, isObject } from "./shared";
+
+export type RadicalTag = "Classical" | "Nelson"
 
 /**
  * A kanji classification based on its radical.
@@ -7,10 +10,27 @@ export interface Radical {
 	/**
 	 * The kind of radical classification
 	 */
-	tag: "Classical" | "Nelson"
+	tag: RadicalTag
 	
 	/**
 	 * The kangxi code for the radical
 	 */
 	radical: Kangxi
+}
+
+export function isRadical(value: unknown): value is Radical {
+	return isObject(value) &&
+		hasStringProperty(value, "tag") &&
+		isRadicalTag(value.tag) &&
+		hasProperty(value, "radical") &&
+		isKangxi(value.radical)
+}
+
+function isRadicalTag(str: string): str is RadicalTag {
+	return str in RADICAL_TAGS
+}
+
+const RADICAL_TAGS: Record<RadicalTag, boolean> = {
+	"Classical": true,
+	"Nelson": true,
 }
