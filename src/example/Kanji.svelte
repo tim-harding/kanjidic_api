1cb33d3a614362eec0e8dc4c260e934844818adf
Tim Harding <Tim@TimHarding.co>
2021-09-03 16:08:57 -0700

    Popover on hover

--- src/example/Kanji.svelte
+++ src/example/Kanji.svelte
@@ -16,12 +16,14 @@
 		<details open={true} class="details">
 			<summary class="summary">
 				<div class="disclosure-triangle">
-					<span class="material-icons-outlined open-disclosure">arrow_right</span>
-					<span class="material-icons-outlined close-disclosure">arrow_drop_down</span>
+					<span class="material-icons-outlined open-disclosure"
+						>arrow_right</span
+					>
+					<span class="material-icons-outlined close-disclosure"
+						>arrow_drop_down</span
+					>
 				</div>
-				<span class="summary-label">
-					Codepoints
-				</span>
+				<span class="summary-label"> Codepoints </span>
 			</summary>
 			<ul class="details-body">
 				{#each character.codepoints as codepoint}
@@ -35,7 +37,7 @@
 <style>
 	.root {
 		margin: 1rem;
-		margin-left: 0.5rem;
+		margin-left: 0rem;
 		grid-template-rows: min-content;
 		gap: 0.75rem;
 	}
@@ -43,46 +45,42 @@
 	.literal {
 		grid-template-columns: min-content 1fr;
 		align-items: flex-end;
-		margin-left: 0.5rem;
+		margin-left: 1.4rem;
 	}
 
 	.summary {
-		grid-template-columns: 1.5rem 1fr;
+		grid-template-columns: 1.4rem 1fr;
 		cursor: pointer;
 		margin-bottom: 0.25rem;
 	}
-	
+
 	.summary-label {
 	}
-	
+
 	.disclosure-triangle {
 		transform: translateY(-0.2rem);
-		/* grid-area: center; */
 		grid-template-areas: "center";
 	}
-	
-	.open-disclosure, .close-disclosure {
+
+	.open-disclosure,
+	.close-disclosure {
 		grid-area: center;
 	}
 
 	.details-body {
 		margin-left: 2rem;
-		gap: 0.25rem;
+		gap: 0.5rem;
 	}
-	
+
 	.close-disclosure {
 		visibility: hidden;
 	}
-	
+
 	.details[open] .close-disclosure {
 		visibility: initial;
 	}
 
 	.details[open] .open-disclosure {
 		visibility: hidden;
-	}
-	
-	.expand-more {
-		transform: translateY(-0.2rem);
 	}
 </style>
--- src/example/Popover.svelte
+++ src/example/Popover.svelte
@@ -1,15 +1,13 @@
-<details class="root">
-	<summary class="material-icons-outlined md-18 summary">
-		info
-	</summary>
+<span class="root">
+	<span class="material-icons-outlined md-18 summary"> info </span>
 	<div class="popover">
 		<div class="left-triangle-base" />
 		<div class="left-triangle-top" />
 		<div class="content">
-			<slot></slot>
+			<slot />
 		</div>
 	</div>
-</details>
+</span>
 
 <style lang="scss">
 	.root {
@@ -23,22 +21,29 @@
 	.summary {
 		cursor: pointer;
 		user-select: none;
+		z-index: 1;
 	}
-	
+
+	.summary:not(:hover) + .popover {
+		opacity: 0;
+		position: absolute;
+		left: -100vw;
+	}
+
 	.popover {
 		position: absolute;
-		left: 1.5rem;
+		left: 1.65rem;
 		grid-template-columns: min-content 1fr;
 		grid-template-areas: "arrow content";
-		transform: translateY(calc(-50% - 0.5rem));
 	}
 
 	.root {
 		position: relative;
 		align-items: center;
 	}
-	
-	.left-triangle-base, .left-triangle-top {
+
+	.left-triangle-base,
+	.left-triangle-top {
 		align-self: center;
 		transform: rotate(45deg);
 		grid-area: arrow;
@@ -48,15 +53,15 @@
 		background-color: var(--snow-storm-2);
 		width: 1.25rem;
 		height: 1.25rem;
-		box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.1);
-		z-index: 1;
+		box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.1);
+		z-index: 2;
 	}
-	
+
 	.left-triangle-top {
 		background-color: var(--snow-storm-0);
 		width: calc(1.25rem - 2px);
 		height: calc(1.25rem - 2px);
-		z-index: 3;
+		z-index: 4;
 	}
 
 	.content {
@@ -68,7 +73,7 @@
 		border-color: var(--snow-storm-2);
 		border-width: 1px;
 		border-style: solid;
-		box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.1);
-		z-index: 2;
+		box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.1);
+		z-index: 3;
 	}
 </style>
