293a4d0d2ab2c49e2d9a0bd0b2b3fe65b12bf956
Tim Harding <Tim@TimHarding.co>
2021-09-05 11:07:19 -0700

    Codepoints popover

--- src/example/Codepoints.svelte
+++ src/example/Codepoints.svelte
@@ -2,12 +2,16 @@
 	import type { Codepoint as CodepointType } from "../lib/codepoint";
 	import KanjiDropdown from "./KanjiDropdown.svelte";
 	import Codepoint from "./Codepoint.svelte";
+	import Popover from "./Popover.svelte";
 
 	export let codepoints: CodepointType[];
 </script>
 
 <KanjiDropdown>
-	<span slot="summary"> Codepoints </span>
+	<span slot="summary" class="text-line">
+		Codepoints&nbsp;
+		<Popover>Alternate encodings for the character.</Popover>
+	</span>
 	<ul slot="content" class="list">
 		{#each codepoints as codepoint}
 			<li>
