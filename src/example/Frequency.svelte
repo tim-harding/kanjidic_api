36248cbceb668e154898041af75ec6334acf330b
Tim Harding <Tim@TimHarding.co>
2021-09-04 22:39:12 -0700

    Information about frequency field

--- src/example/Frequency.svelte
+++ src/example/Frequency.svelte
@@ -1,16 +1,25 @@
 <script lang="ts">
+	import Popover from "./Popover.svelte";
+
 	export let frequency: number;
 </script>
 
-<dl	class="text-line root">
-	<dt>
-		Frequency
-	</dt>
-	:&nbsp;
-	<dd>
-		{frequency}
-	</dd>
-</dl>
+<div class="text-line root">
+	<dl class="passthrough">
+		<dt>Frequency</dt>
+		:&nbsp;
+		<dd>
+			{frequency}
+		</dd>
+	</dl>
+	&nbsp;
+	<Popover>
+		<p>
+			A ranking of how often the character appears in newspapers, 1 being the
+			most frequent.
+		</p>
+	</Popover>
+</div>
 
 <style>
 	.root {
