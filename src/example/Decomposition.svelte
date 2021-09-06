86f2665e08c75566b9923547f76c5e189d37bd60
Tim Harding <Tim@TimHarding.co>
2021-09-05 20:08:23 -0700

    Displaying the decomposition

---
+++ src/example/Decomposition.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+import Popover from "./Popover.svelte";
+
+export let decomposition: string[];
+</script>
+
+<dl class="text-line root">
+	<dt>
+		Decomposition
+	</dt>
+	:&nbsp;
+	<dd>
+		<ul class="radicals">
+			{#each decomposition as radical}
+				<li class="radical noto">
+					{radical}
+				</li>
+			{/each}
+		</ul>
+	</dd>
+	&nbsp;
+	<Popover>
+		The constituent radicals in the kanji.
+	</Popover>
+</dl>
+
+<style lang="scss">
+	.root {
+		margin-left: 1.4rem;
+	}
+	
+	.radicals {
+		grid-auto-flow: column;
+	}
+	
+	.radical {
+		grid-template-columns: max-content max-content;
+		align-items: flex-end;
+		font-weight: 700;
+
+		&::after {
+			content: ", ";
+			white-space: pre;
+			font-family: "Roboto";
+			font-weight: 400;
+		}
+		
+		&:last-of-type::after {
+			content: none;
+		}
+	}
+</style>
--- src/example/Kanji.svelte
+++ src/example/Kanji.svelte
@@ -14,6 +14,7 @@
 	import Readings from "./Readings.svelte";
 	import Translations from "./Translations.svelte";
 	import Nanori from "./Nanori.svelte";
+	import Decomposition from "./Decomposition.svelte";
 
 	export let character: Character;
 </script>
@@ -58,6 +59,9 @@
 	{/if}
 	{#if character.nanori !== undefined}
 		<Nanori nanori={character.nanori} />
+	{/if}
+	{#if character.decomposition !== undefined}
+		<Decomposition decomposition={character.decomposition} />
 	{/if}
 </div>
 
