c4fd8d171f2c2544ae3fc3fbf486943b5b376673
Tim Harding <Tim@TimHarding.co>
2021-09-07 20:22:29 -0700

    Reworked stroke count display

--- src/example/StrokeCounts.svelte
+++ src/example/StrokeCounts.svelte
@@ -1,56 +1,41 @@
 <script lang="ts">
 	import type { StrokeCount } from "../lib";
-	import KanjiDropdown from "./KanjiDropdown.svelte";
 	import Popover from "./Popover.svelte";
 
 	export let strokeCounts: StrokeCount;
 </script>
 
-{#if strokeCounts.miscounts !== undefined}
-	<KanjiDropdown>
-		<span slot="summary" class="text-line">
-			Stroke counts&nbsp;
-			<Popover>The number of strokes it takes to draw the kanji.</Popover>
-		</span>
-		<dl slot="content">
-			<div class="text-line">
-				<dt>Accepted</dt>
-				:&nbsp;
-				<dd>
-					{strokeCounts.accepted}
-				</dd>
-			</div>
-
-			<div class="text-line">
-				<dt>Miscounts</dt>
-				:
-				<dd class="transparent">
-					<ul class="text-line">
-						{#each strokeCounts.miscounts as miscount}
-							<li class="miscount">
-								&nbsp;{miscount}
-							</li>
-						{/each}
-					</ul>
-				</dd>
-			</div>
-		</dl>
-	</KanjiDropdown>
-
-{:else}
 <div class="root text-line">
 	<dl class="passthrough">
-		<dt>Stroke counts</dt>
+		<dt>Stroke count</dt>
 		:&nbsp;
 		<dd>
 			{strokeCounts.accepted}
 		</dd>
 	</dl>
 	&nbsp;
-	<Popover>The number of strokes it takes to draw the kanji.</Popover>
+	<Popover>
+		The number of strokes it takes to draw the kanji.
+		{#if strokeCounts.miscounts !== undefined}
+			<dl>
+				<div class="text-line">
+					<dt>Miscounts</dt>
+					:&nbsp;
+					<dd class="transparent">
+						<ul class="text-line">
+							{#each strokeCounts.miscounts as miscount}
+								<li class="miscount">
+									&nbsp;{miscount}
+								</li>
+							{/each}
+						</ul>
+					</dd>
+				</div>
+			</dl>
+		{/if}
+	</Popover>
 </div>
 
-{/if}
 <style lang="scss">
 	.root {
 		margin-left: 1.4rem;
