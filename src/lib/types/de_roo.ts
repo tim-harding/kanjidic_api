3a4590fbf712152b40cfb0eb730736a01aaf7884
Tim Harding <Tim@TimHarding.co>
2021-09-09 11:19:35 -0700

    Refactored ExtremeBottom

--- src/lib/types/de_roo.ts
+++ src/lib/types/de_roo.ts
@@ -1,5 +1,5 @@
 import { ExtremeTop, isExtremeTop } from "./extreme_top";
-import { ExtremeBottom, isExtremeBottom } from "./extreme_bottom";
+import { ExtremeBottom} from "./extreme_bottom";
 import { hasProperty, isObject } from "../shared";
 
 export namespace DeRoo {
@@ -17,7 +17,7 @@ export namespace DeRoo {
     /**
      * The graphic element at the bottom right of a kanji.
      */
-    bottom: ExtremeBottom;
+    bottom: ExtremeBottom.ExtremeBottom;
   }
 
   /**
@@ -35,7 +35,7 @@ export namespace DeRoo {
       hasProperty(value, "top") &&
       isExtremeTop(value.top) &&
       hasProperty(value, "bottom") &&
-      isExtremeBottom(value.bottom)
+      ExtremeBottom.check(value.bottom)
     );
   }
 }
--- src/lib/types/extreme_bottom.ts
+++ src/lib/types/extreme_bottom.ts
@@ -1,70 +1,74 @@
-import { isInt } from "./uint"
+import { isInt } from "./uint";
 
-/**
- * The graphic element at the bottom right of a kanji.
- */
-export const enum ExtremeBottom {
-	// Dot
-	FourDots = 40,
-	Small,
-	Water,
+export namespace ExtremeBottom {
+  /**
+   * The graphic element at the bottom right of a kanji.
+   */
+  export const enum ExtremeBottom {
+    // Dot
+    FourDots = 40,
+    Small,
+    Water,
 
-	// Left hook
-	KanaRi,
-	Seal,
-	SwordBottom,
-	Moon,
-	DotlessInch,
-	Inch,
-	MouthLeftHook,
-	BirdBottom,
-	Animal,
-	BowBottom,
-	LeftHook,
+    // Left hook
+    KanaRi,
+    Seal,
+    SwordBottom,
+    Moon,
+    DotlessInch,
+    Inch,
+    MouthLeftHook,
+    BirdBottom,
+    Animal,
+    BowBottom,
+    LeftHook,
 
-	// Vertical line
-	VerticalLine,
-	Cross,
+    // Vertical line
+    VerticalLine,
+    Cross,
 
-	// Right hook
-	RightHook,
-	Legs,
-	Heart,
-	TasseledSpearBottom,
+    // Right hook
+    RightHook,
+    Legs,
+    Heart,
+    TasseledSpearBottom,
 
-	// Diagonal line
-	KanaNo,
+    // Diagonal line
+    KanaNo,
 
-	// Back diagonal line
-	SmallPodium,
-	BackKanaNo,
-	Big,
-	Tree,
-	SmallSpoon,
-	Govern,
-	Again,
-	WindyAgain,
-	Woman,
+    // Back diagonal line
+    SmallPodium,
+    BackKanaNo,
+    Big,
+    Tree,
+    SmallSpoon,
+    Govern,
+    Again,
+    WindyAgain,
+    Woman,
 
-	// Head bottom
-	HeadBottom,
+    // Head bottom
+    HeadBottom,
 
-	// Watakushi bottom
-	WatakushiBottom,
+    // Watakushi bottom
+    WatakushiBottom,
 
-	// Horizontal line
-	HorizontalLine,
-	StandingBottom,
-	DishBottom,
-	BottomCorner,
-	Mountain,
-	Mouth,
-	Sun,
-	Eye,
-}
+    // Horizontal line
+    HorizontalLine,
+    StandingBottom,
+    DishBottom,
+    BottomCorner,
+    Mountain,
+    Mouth,
+    Sun,
+    Eye,
+  }
 
-export function isExtremeBottom(value: unknown): value is ExtremeBottom {
-	return isInt(value) &&
-		value >= ExtremeBottom.FourDots &&
-		value <= ExtremeBottom.Eye
+  export function check(value: unknown): value is ExtremeBottom {
+    return (
+      isInt(value) &&
+      value >= ExtremeBottom.FourDots &&
+      value <= ExtremeBottom.Eye
+    );
+  }
 }
